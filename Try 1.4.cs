3c56e80f59c42fa09a944cdedde5c769bc6cbe4e
Steinest <emikasims@mail.ru>
2025-06-15 01:31:35 +0300

    Add files via upload

---
+++ Try 1.4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите три целых числа:");
+
+        int num1 = GetIntFromConsole("Первое число: ");
+        int num2 = GetIntFromConsole("Второе число: ");
+        int num3 = GetIntFromConsole("Третье число: ");
+
+        int result = 0;
+
+        //Проходим по каждому биту
+        for (int i = 0; i < 32; i++)
+        {
+            //Извлекаем i-й бит
+            int bit1 = (num1 >> i) & 1;
+            int bit2 = (num2 >> i) & 1;
+            int bit3 = (num3 >> i) & 1;
+
+            //Считаем, сколько раз встречается 1
+            int onesCount = bit1 + bit2 + bit3;
+
+            //Определяем наиболее частый бит
+            int mostBit = (onesCount >= 2) ? 1 : 0;
+
+            //Устанавливаем i-й бит в результате
+            if (mostBit == 1)
+            {
+                result |= (1 << i);
+            }
+        }
+
+        Console.WriteLine($"Результирующее число: {result}");
+    }
+
+    //Вспомогательная функция
+    static int GetIntFromConsole(string prompt)
+    {
+        int number;
+        while (true)
+        {
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                return number;
+            }
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+        }
+    }
+}
---
+++ Try 2.2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+public class FactorialComparison
+{
+    public static long RecursiveFactorial(int n)
+    {
+        if (n <= 1) return 1;
+        return n * RecursiveFactorial(n - 1);
+    }
+
+    public static long IterativeFactorial(int n)
+    {
+        long result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для вычисления факториала:");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nРезультаты вычисления:");
+        Console.WriteLine($"Рекурсивно: {RecursiveFactorial(number)}");
+        Console.WriteLine($"Итеративно: {IterativeFactorial(number)}");
+    }
+}
